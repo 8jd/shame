c9522c3dcc32a748ee5f015e3871233c78369829
8jd <84992744+8jd@users.noreply.github.com>
2024-12-05 17:14:55 -0500

    Update index.js

--- x/scripts/index.js
+++ x/scripts/index.js
@@ -69,13 +69,13 @@ function memberCall(pfpSrc, textContent, user) {
         const pfpImage = document.getElementById("pfp-image");
 
         displayText.innerHTML = textContent;
-        document.title = "@fail; " + user;
+        document.title = "@exile; " + user;
         pfpImage.src = pfpSrc;
 
         transitionActive = false;
     } else {
         current_page = "member";
-        document.title = "@fail; " + user;
+        document.title = "@exile; " + user;
         const displayText = document.querySelector(".display-text");
         const randomGif = document.getElementById("random-gif");
         const pfpImage = document.getElementById("pfp-image");
@@ -92,38 +92,29 @@ function memberCall(pfpSrc, textContent, user) {
     }
 }
 
-function lsd() {
+function liam() {
     memberCall(
-        "assets/lsd_icon.png",
-        "<a href='https://solo.to/com' target='_blank' style='color: white;'>solo.to</a>",
+        "assets/liam_icon.png",
+        "<a href='numb.lol' target='_blank' style='color: white;'>numb</a>, <a href='https://instagram.com/cyxt' target='_blank' style='color: white;'>instagram</a>",
         "lsd"
     );
 }
 
-function rainku() {
+function lain() {
     memberCall(
-        "assets/rainku_icon.png",
-        "<a href='https://snapchat.com/@fedthreat' target='_blank' style='color: white;'>snapchat</a>, <a href='https://instagram.com/xvurhi' target='_blank' style='color: white;'>instagram</a>, <a href='https://t.me/hadence' target='_blank' style='color: white;'>telegram</a>",
+        "assets/lain_icon.png",
+        "<a href='https://namemc.com/_2' target='_blank' style='color: white;'>namemc</a>, <a href='https://instagram.com/axst' target='_blank' style='color: white;'>instagram</a>, <a href='https://x.com/eternallyexile' target='_blank' style='color: white;'>x</a>",
         "rainku"
     );
 }
 
-function valexnt() {
+function op() {
     memberCall(
-        "assets/valexnt_icon.png",
-        "<a href='https://discord.com/users/1166703542468358234' target='_blank' style='color: white;'>discord</a>, <a href='https://t.me/valexntt' target='_blank' style='color: white;'>telegram</a>",
+        "assets/op_icon.png",
+        "<a href='https://discord.com/users/1125520697100345445' target='_blank' style='color: white;'>discord</a>, <a href='https://instagram.com/2facd' target='_blank' style='color: white;'>instagram</a>",
         "valexnt"
     );
 }
-
-function loundra() {
-    memberCall(
-        "assets/l7_icon.png",
-        "<a href='https://solo.to/l7' target='_blank' style='color: white;'>solo.to</a>",
-        "l7"
-    );
-}
-
 
 // CORE BUTTONS
 
@@ -136,13 +127,13 @@ function memberCall(pfpSrc, textContent, user) {
         const pfpImage = document.getElementById("pfp-image");
 
         displayText.innerHTML = textContent;
-        document.title = "@fail; " + user;
+        document.title = "@exile; " + user;
         pfpImage.src = pfpSrc;
 
         transitionActive = false;
     } else {
         current_page = "member";
-        document.title = "@fail; " + user;
+        document.title = "@exile; " + user;
         const displayText = document.querySelector(".display-text");
         const randomGif = document.getElementById("random-gif");
         const pfpImage = document.getElementById("pfp-image");
@@ -161,7 +152,7 @@ function memberCall(pfpSrc, textContent, user) {
 function updatePage(htmlContent, pageTitle) {
     if (transitionActive) return;
     transitionActive = true;
-    document.title = "@fail; " + pageTitle;
+    document.title = "@exile; " + pageTitle;
     const displayText = document.querySelector(".display-text");
 
     if (current_page !== "main") {
@@ -183,25 +174,17 @@ function updatePage(htmlContent, pageTitle) {
     }
 }
 
-
-function domains() {
-    updatePage(
-        "<a href='https://masochi.st/' target='_blank' style='color: white;'>masochi.st</a>, <a href='https://doxe.rs/' target='_blank' style='color: white;'>doxe.rs</a>, <a href='https://swatte.rs/' target='_blank' style='color: white;'>swatte.rs</a>",
-        "projects"
-    );
-}
-
 function about() {
     updatePage(
-        "<a href='https://discord.gg/fail' target='_blank' style='color: white;'>@fail</a> is a cyber collective with a primary focus on coding and various other digital endeavors.",
+        "<a href='https://shame.lol' target='_blank' style='color: white;'>@exile</a> is a cyber collective with a primary focus on coding and various other digital endeavors.",
         "about"
     );
 }
 
 
 
-function discord() {
-    window.open("https://discord.gg/fail", "_blank");
+function site() {
+    window.open("https://shame.lol", "_blank");
 }
 
 
@@ -210,7 +193,7 @@ function discord() {
 
 function getRandomSentence() {
     const sentences = [
-        "@ <a href='https://discord.gg/fail' target='_blank' style='color: white;'>fail</a>, a collective"
+        "@ <a href='https://shame.lol' target='_blank' style='color: white;'>exile</a>, a collective"
     ];
     const randomIndex = Math.floor(Math.random() * sentences.length);
     return sentences[randomIndex];
